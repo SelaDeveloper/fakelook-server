88a8bec87ebb51230bd0a84004242f113c2ecfe0
Alexey <hello@gmail.com>
2022-03-14 09:50:38 +0200

    gfdf

--- fakeLook-starter/Program.cs
+++ fakeLook-starter/Program.cs
@@ -14,7 +14,7 @@ namespace fakeLook_starter
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-            Console.WriteLine("dsd");
+            Console.WriteLine("Hello");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
