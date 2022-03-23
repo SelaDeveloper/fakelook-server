3fe496cf2f56702fb7a638039145e550d48f5f73
itamar <itamar.BemShimon@gmail.com>
2022-03-23 09:22:25 +0200

    fix bugs of filter

--- fakeLook-models/Models/PostFilter.cs
+++ fakeLook-models/Models/PostFilter.cs
@@ -17,10 +17,8 @@ namespace fakeLook_models.Models
 
         public bool checkTaggedUsers(ICollection<UserTaggedPost> postTaggedUsers)
         {
-            if (TaggedUsers == null || TaggedUsers.Count == 0)
-            {
-                return true;
-            }
+            if (TaggedUsers == null || TaggedUsers.Count == 0) return true;
+            if (TaggedUsers.Count == 1 && TaggedUsers.First().Equals("")) return true;
             if (postTaggedUsers == null || postTaggedUsers.Count == 0) return false;
             foreach (var filterTaggedUser in TaggedUsers)
             {
@@ -37,10 +35,8 @@ namespace fakeLook_models.Models
 
         public bool checkTaggs(ICollection<Tag> postTags)
         {
-            if (Tags == null || Tags.Count == 0 )
-            {
-                return true;
-            }
+            if (Tags == null || Tags.Count == 0) return true;
+            if (Tags.Count == 1 && Tags.First().Equals("")) return true;
             if (postTags == null || postTags.Count == 0) return false;
             foreach (var tag in Tags)
             {
@@ -57,8 +53,8 @@ namespace fakeLook_models.Models
 
         public bool checkDate(DateTime postDate)
         {
-            if (startDate.Equals(new DateTime(2000,01,01)) || endDate.Equals(new DateTime(2000, 01, 01))) return true;
-            if (postDate >= startDate && postDate <= endDate )
+            if (startDate.Equals(new DateTime(2000, 01, 01)) || endDate.Equals(new DateTime(2000, 01, 01))) return true;
+            if (postDate >= startDate && postDate <= endDate)
                 return true;
             return false;
         }
@@ -66,6 +62,7 @@ namespace fakeLook_models.Models
         public bool checkPublishers(string UserName)
         {
             if (Publishers == null || Publishers.Count == 0) return true;
+            if (Publishers.Count == 1 && Publishers.First().Equals("")) return true;
             if (Publishers.Contains(UserName)) return true;
             return false;
         }
